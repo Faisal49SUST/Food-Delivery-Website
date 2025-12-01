d826aecf90a268243a67ff2c276faa38ea254580
Faisal49SUST <faisalahmed3205@gmail.com>
2025-12-02 01:55:12 +0600

    Update Add.jsx

--- Khabar---A-Food-Delivery-Website-main/admin/src/pages/Add/Add.jsx
+++ Khabar---A-Food-Delivery-Website-main/admin/src/pages/Add/Add.jsx
@@ -10,7 +10,7 @@ import {useNavigate } from "react-router-dom";
 
 const Add = () => {
 
-  const url ="https://khabar-a-food-delivery-website-backend-kmqo.onrender.com";
+  const url ="https://food-delivery-website-backend-89kv.onrender.com";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
